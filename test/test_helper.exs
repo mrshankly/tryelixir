43a994b966b1853a1bbfbe9f0ec8ea0644465fb5
mrshankly <mrshankly@riseup.net>
2013-07-01 07:15:16 +0100

    Added some tests.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,31 @@
 ExUnit.start
+
+defmodule Tryelixir.Case do
+  @moduledoc false
+
+  defmacro __using__(_) do
+    quote do
+      use ExUnit.Case
+      import unquote(__MODULE__)
+    end
+  end
+
+  @doc """
+  Runs Tryelixir.Eval eval_loop, feeds the provided input and returns the
+  produced output. The header and whitespaces are stripped.
+  """
+  def capture_output(input) do
+    ExUnit.CaptureIO.capture_io([input: input, capture_prompt: false],
+      fn -> Tryelixir.Eval.start() end) |> strip_output
+  end
+
+  defp strip_output(string) do
+    string
+    |> strip_line # strip the greeting
+    |> String.strip
+  end
+
+  defp strip_line(string) do
+    Regex.replace %r/\A.+?$/ms, string, ""
+  end
+end
--- test/tryelixir_test.exs
+++ test/tryelixir_test.exs
@@ -1,9 +1,15 @@
 Code.require_file "test_helper.exs", __DIR__
 
 defmodule TryelixirTest do
-  use ExUnit.Case
+  use Tryelixir.Case
 
-  test "the truth" do
-    assert(true)
+  @restricted "** (RuntimeError) restricted"
+
+  test "normal input" do
+    assert capture_output("1 + 1") == "2"
+  end
+
+  test "restricted module" do
+  	assert capture_output("IO.puts \"Hello world!\"") == @restricted
   end
 end
