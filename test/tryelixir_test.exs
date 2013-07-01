485fa3bd09e3a996390ccad18db7e7ce2282a163
mrshankly <mrshankly@riseup.net>
2013-07-01 08:34:22 +0100

    Added tests for fn's and local functions.

--- test/tryelixir_test.exs
+++ test/tryelixir_test.exs
@@ -9,7 +9,23 @@ defmodule TryelixirTest do
     assert capture_output("1 + 1") == "2"
   end
 
+  test "allowed module" do
+  	assert capture_output("Enum.map([1,2,3], &1 * 2)") == "[2,4,6]"
+  end
+
+  test "allowed module with fn" do
+  	assert capture_output("Enum.map([1,2,3], fn(x) -> x + 1 end)") == "[2,3,4]"
+  end
+
   test "restricted module" do
   	assert capture_output("IO.puts \"Hello world!\"") == @restricted
+  end
+
+  test "restricted fn" do
+  	assert capture_output("fn -> System.cmd(pwd) end") == @restricted
+  end
+
+  test "restricted local function" do
+  	assert capture_output("spawn(fn -> 1 + 1 end)") == @restricted
   end
 end
