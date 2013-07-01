90982d80ddee68afb6bcb2d3303734fbfcd413de
mrshankly <mrshankly@riseup.net>
2013-07-01 08:40:23 +0100

    Fixed identation.

--- test/tryelixir_test.exs
+++ test/tryelixir_test.exs
@@ -10,22 +10,22 @@ defmodule TryelixirTest do
   end
 
   test "allowed module" do
-  	assert capture_output("Enum.map([1,2,3], &1 * 2)") == "[2,4,6]"
+    assert capture_output("Enum.map([1,2,3], &1 * 2)") == "[2,4,6]"
   end
 
   test "allowed module with fn" do
-  	assert capture_output("Enum.map([1,2,3], fn(x) -> x + 1 end)") == "[2,3,4]"
+    assert capture_output("Enum.map([1,2,3], fn(x) -> x + 1 end)") == "[2,3,4]"
   end
 
   test "restricted module" do
-  	assert capture_output("IO.puts \"Hello world!\"") == @restricted
+    assert capture_output("IO.puts \"Hello world!\"") == @restricted
   end
 
   test "restricted fn" do
-  	assert capture_output("fn -> System.cmd(pwd) end") == @restricted
+    assert capture_output("fn -> System.cmd(pwd) end") == @restricted
   end
 
   test "restricted local function" do
-  	assert capture_output("spawn(fn -> 1 + 1 end)") == @restricted
+    assert capture_output("spawn(fn -> 1 + 1 end)") == @restricted
   end
 end
