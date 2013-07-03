02a298a4a7c8624057e86bc9a1cff3da041d2dd0
mrshankly <mrshankly@riseup.net>
2013-07-03 23:20:54 +0100

    Fixed identation.

--- priv/static/js/tryelixir.js
+++ priv/static/js/tryelixir.js
@@ -1,73 +1,73 @@
 var tutorialActive = false;
 var currentPage = 0;
 var tutorialPages = [
-	"t1.html",
-	"t2.html",
-	"t3.html"
+    "t1.html",
+    "t2.html",
+    "t3.html"
 ];
 
 function changeTutorial(index) {
-	$("#tutorial").fadeOut("fast", function() {
-		$("#tutorial").load("tutorial/" + tutorialPages[index]);
-		$("#tutorial").fadeIn("fast");
-	});
+    $("#tutorial").fadeOut("fast", function() {
+        $("#tutorial").load("tutorial/" + tutorialPages[index]);
+        $("#tutorial").fadeIn("fast");
+    });
 }
 
 function goToPage(number) {
-	currentPage = number;
-	changeTutorial(number);
+    currentPage = number;
+    changeTutorial(number);
 }
 
 function onValidate(input) {
-	return (input != "");
+    return (input != "");
 }
 
 function onHandle(line, report) {
-	switch (line) {
-		case ":next":
-			if (tutorialActive && currentPage < tutorialPages.length - 1)
-				goToPage(currentPage + 1);
-			return true;
-		case ":prev":
-			if (tutorialActive && currentPage > 0)
-				goToPage(currentPage - 1);
-			return true;
-		case ":restart":
-			if (tutorialActive)
-				goToPage(0);
-			return true;
-		case ":clear":
-			shell();
-			return true;
-		case ":start":
-			tutorialActive = true;
-			goToPage(0);
-			return true;
-		default:
-			return line;
-	}
-	if (line == ":clear") {
-		shell();
-	} else {
-		return line;
-	}
+    switch (line) {
+        case ":next":
+            if (tutorialActive && currentPage < tutorialPages.length - 1)
+                goToPage(currentPage + 1);
+            return true;
+        case ":prev":
+            if (tutorialActive && currentPage > 0)
+                goToPage(currentPage - 1);
+            return true;
+        case ":restart":
+            if (tutorialActive)
+                goToPage(0);
+            return true;
+        case ":clear":
+            shell();
+            return true;
+        case ":start":
+            tutorialActive = true;
+            goToPage(0);
+            return true;
+        default:
+            return line;
+    }
+    if (line == ":clear") {
+        shell();
+    } else {
+        return line;
+    }
 }
 
 function shell() {
-	$("#console").empty();
-	var console = $("#console");
-	var controller = console.console({
-		promptLabel: "iex(1)> ",
-		commandValidate: onValidate,
-		commandHandle: onHandle,
-		autofocus: true,
-		animateScroll: true,
-		promptHistory: true,
-		welcomeMessage: "Interactive Elixir (0.9.4-dev) - (type h() ENTER for help)"
-	});
+    $("#console").empty();
+    var console = $("#console");
+    var controller = console.console({
+        promptLabel: "iex(1)> ",
+        commandValidate: onValidate,
+        commandHandle: onHandle,
+        autofocus: true,
+        animateScroll: true,
+        promptHistory: true,
+        welcomeMessage: "Interactive Elixir (0.9.4-dev) - (type h() ENTER for help)"
+    });
 }
 
 $(document).ready(function() {
-	$("#tutorial").load("intro.html");
-	shell();
+    $("#tutorial").load("intro.html");
+    shell();
 });
