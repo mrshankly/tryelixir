6330900eb9edba6d1f23cb28d0e0aab42076964f
mrshankly <mrshankly@riseup.net>
2013-07-04 11:11:04 +0100

    Project rename.

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Tryelixir.Mixfile do
   use Mix.Project
 
   def project do
-    [ app: :'tryelixir.org',
+    [ app: tryelixir,
       version: "0.0.1",
       elixir: "~> 0.9.4-dev",
       deps: deps ]
