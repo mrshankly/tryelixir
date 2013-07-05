8534a0da7ae30cdf2a5149df1e72f18e6ab12867
mrshankly <mrshankly@riseup.net>
2013-07-05 02:32:05 +0100

    Request eval loop and added function that formats the output.

--- web/routers/api_router.ex
+++ web/routers/api_router.ex
@@ -2,10 +2,29 @@ defmodule ApiRouter do
   use Dynamo.Router
 
   prepare do
-    conn.fetch :params
+    conn.fetch [:cookies, :params]
   end
 
   post "/eval" do
-    conn.resp(200, "#{conn.params[:code]}")
+  	pid = Dynamo.HTTP.Cookies.get_cookie(conn, :eval_pid)
+  	|> Tryelixir.Cookie.decode |> list_to_pid
+
+  	pid <- {self, {:input, conn.params[:code]}}
+  	resp = receive do
+  		response ->
+  			response
+  	end
+
+    conn.resp(200, format_json(resp))
+  end
+
+  defp format_json({prompt, {"error", result}}) do
+  	%b/{"prompt":"#{prompt}","type":"error","result":"#{result}"}/
+  end
+
+  defp format_json({prompt, {type, result}}) do
+  	# show double-quotes in strings
+  	result = String.replace("#{inspect result}", "\"", "\\\"")
+  	%b/{"prompt":"#{prompt}","type":"#{type}","result":"#{result}"}/
   end
 end
