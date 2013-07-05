710b6f9d0c6183819ed0c1f8bc33cd6f4e6f376f
mrshankly <mrshankly@riseup.net>
2013-07-05 11:36:51 +0100

    Moved lib tests.

---
+++ test/tryelixir/test_helper.exs
@@ -0,0 +1,28 @@
+ExUnit.start
+
+defmodule Tryelixir.Case do
+  @moduledoc false
+
+  defmacro __using__(_) do
+    quote do
+      use ExUnit.Case
+      import unquote(__MODULE__)
+    end
+  end
+
+  def start_eval() do
+    pid = Tryelixir.Eval.start
+    |> Process.register(:test_eval)
+  end
+
+  @doc """
+  Sends the input to a eval_loop process and returns the response.
+  """
+  def test_eval(input) do
+    :test_eval <- {self, {:input, input}}
+    receive do
+      response ->
+        response
+    end
+  end
+end
