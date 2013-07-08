bd67f335951893f62654e07d7a215624275244eb
mrshankly <mrshankly@riseup.net>
2013-07-08 22:22:35 +0100

    Get dependencies.

--- docker-run.sh
+++ docker-run.sh
@@ -4,4 +4,4 @@
 cd /opt
 git clone https://github.com/elixir-lang/elixir; cd elixir; make; cd ..
 export PATH=`pwd`/elixir/bin:$PATH
-git clone https://github.com/tryelixir/tryelixir; cd tryelixir; MIX_ENV=prod mix server
+git clone https://github.com/tryelixir/tryelixir; cd tryelixir; MIX_ENV=prod mix do deps.get, server
