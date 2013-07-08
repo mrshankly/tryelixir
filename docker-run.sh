d231ec5f214f3c781406323a52912d7adb443ee3
mrshankly <mrshankly@riseup.net>
2013-07-08 22:05:57 +0100

    Docker setup.

---
+++ docker-run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# I'm using this to run tryelixir with docker, you need erlang installed
+cd /opt
+git clone https://github.com/elixir-lang/elixir; cd elixir; make; cd ..
+export PATH=`pwd`/elixir/bin:$PATH
+git clone https://github.com/tryelixir/tryelixir; cd tryelixir; MIX_ENV=prod mix server
