c34b1b25d97a2188ab238f8b43830a02c256acf1
João Marques <jm@mrshankly.eu>
2014-10-18 13:03:32 +0100

    Update watcher module Add remove/1 and spawn/3 and clean the code a little.

--- lib/tryelixir/watcher.ex
+++ lib/tryelixir/watcher.ex
@@ -1,5 +1,15 @@
 defmodule Tryelixir.Watcher do
-  @moduledoc false
+  @moduledoc """
+  Responsible for limiting the number of active processes from a certain pid.
+
+  It uses 2 ETS tables to keep track of everything. One table keeps track of
+  the "parent" pids and their active process count. The second table has all
+  the monitor references of the active processes relative to a certain
+  "parent" process.
+
+  A process should be created with either `Tryelixir.Watcher.spawn/1` or
+  `Tryelixir.Watcher.spawn/3`.
+  """
 
   use GenServer
 
@@ -7,18 +17,32 @@ defmodule Tryelixir.Watcher do
 
   # API
 
+  @spec start_link() :: {:ok, pid} | {:error, term}
   def start_link() do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
 
+  @spec add(pid) :: :ok
   def add(pid) do
     GenServer.cast(__MODULE__, {:add, pid})
   end
 
+  @spec remove(pid) :: :ok
+  def remove(pid) do
+    GenServer.cast(__MODULE__, {:remove, pid})
+  end
+
+  @spec spawn(pid, (() -> any)) :: {:ok, pid} | {:error, term}
   def spawn(pid, fun) do
     GenServer.call(__MODULE__, {:spawn, pid, fun})
   end
 
+  @spec spawn(pid, module, atom, list) :: {:ok, pid} | {:error, term}
+  def spawn(pid, mod, fun, args) do
+    GenServer.call(__MODULE__, {:spawn, pid, {mod, fun, args}})
+  end
+
+  @spec max() :: non_neg_integer
   def max(), do: @max
 
   # GenServer callbacks.
@@ -29,17 +53,16 @@ defmodule Tryelixir.Watcher do
     {:ok, {ps, rs}}
   end
 
-  def handle_call({:spawn, pid, fun}, _from, {ps, rs}) do
-    max = @max + 1
-    new = :ets.update_counter(ps, pid, {2, 1, max, max})
+  def handle_call({:spawn, pid, spec}, _from, {ps, rs}) do
+    pcount = :ets.update_counter(ps, pid, {2, 1, @max, @max+1})
 
-    reply = if new < max do
-      {p, ref} = spawn_monitor(fun)
-      :ets.insert(rs, {ref, pid})
-      {:ok, p}
-    else
-      :ets.update_counter(ps, pid, {2, -1, 0, 0})
+    reply = if pcount > @max do
+      :ets.insert(ps, {pid, @max})
       {:error, :limit}
+    else
+      {p, r} = smonitor(spec)
+      :ets.insert(rs, {r, pid, p})
+      {:ok, p}
     end
 
     {:reply, reply, {ps, rs}}
@@ -52,6 +75,12 @@ defmodule Tryelixir.Watcher do
   def handle_cast({:add, pid}, tables = {ps, _}) do
     :ets.insert(ps, {pid, 0})
     {:noreply, tables}
+  end
+
+  def handle_cast({:remove, pid}, {ps, rs}) do
+    :ets.delete(ps, pid)
+    :ets.match(rs, {:"$1", pid, :"$2"}) |> Enum.each(&remove_ref(rs, &1))
+    {:noreply, {ps, rs}}
   end
 
   def handle_cast(_msg, tables) do
@@ -68,4 +97,16 @@ defmodule Tryelixir.Watcher do
   def handle_info(_msg, tables) do
     {:noreply, tables}
   end
+
+  # Helpers
+
+  defp smonitor({m, f, a}), do: spawn_monitor(m, f, a)
+  defp smonitor(f), do: spawn_monitor(f)
+
+  defp remove_ref(tid, [ref, pid]) do
+    Process.demonitor(ref, [:flush])
+    Process.exit(pid, :kill)
+    :ets.delete(tid, ref)
+  end
+  defp remove_ref(_tid, _rp), do: :ok
 end
